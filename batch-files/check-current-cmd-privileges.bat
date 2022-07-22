bf8a3f391afe9b041b006aeca5fed90f2b2b1d78
Manu <m.fuica01@ufromail.cl>
2022-07-22 11:03:32 -0400

    chore(batch): update file

--- batch-files/check-current-cmd-privileges.bat
+++ batch-files/check-current-cmd-privileges.bat
@@ -1,1 +1,14 @@
-test
+@echo off
+goto check_Permissions
+
+:check_Permissions
+    echo Administrative permissions required. Detecting permissions...
+
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Success: Administrative permissions confirmed.
+    ) else (
+        echo Failure: Current permissions inadequate.
+    )
+
+    pause >nul
